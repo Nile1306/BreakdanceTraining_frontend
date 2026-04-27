dd164380f28d2c2818f47cec7ba1679461fd79ec
Ni Le <ni.le13062013@gmail.com>
2026-04-27 16:08:49 +0200

    M2

---
+++ src/components/DrillComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Footwork Sessions</h2>
+    <div v-for="session in sessions" :key="session.round">
+      Work: {{ session.workTime }}s |
+      Rest: {{ session.restTime }}s |
+      Rounds: {{ session.round }}
+    </div>
+    <button @click="start">Start</button>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const sessions = ref([
+  { workTime: 40, restTime: 20, round: 3 }
+])
+function start() {
+  alert("Start training!")
+}
+</script>
