bd995feecc21bde14f6206a43191df3e50394fd8
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2026-05-12 19:41:25 +0200

    pre render deployment

--- src/components/DrillComponent.vue
+++ src/components/DrillComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Footwork Sessions</h2>
-    <div v-for="session in sessions" :key="session.round">
+    <div v-if="session">
       Work: {{ session.workTime }}s |
       Rest: {{ session.restTime }}s |
       Rounds: {{ session.round }}
@@ -9,11 +9,20 @@
     <button @click="start">Start</button>
   </div>
 </template>
+
 <script setup>
-import { ref } from 'vue'
-const sessions = ref([
-  { workTime: 40, restTime: 20, round: 3 }
-])
+import { ref, onMounted } from 'vue'
+
+const session = ref(null)
+
+onMounted(() => {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  fetch(`${baseUrl}/session`)
+    .then(response => response.json())
+    .then(data => { session.value = data })
+    .catch(error => console.log('error', error))
+})
+
 function start() {
   alert("Start training!")
 }
