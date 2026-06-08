952906d7245e2f57d6de42aa48bc29a604fcbd60
Ni Le <ni.le13062013@gmail.com>
2026-06-08 10:51:33 +0200

    Improve statistic.vue

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,10 +1,41 @@
+<script setup>
+import { ref, onMounted, onActivated } from 'vue'
+
+const stats = ref([])
+async function loadData() {
+  console.log("CALL API") // debug
+
+  const res = await fetch("http://localhost:8080/sessions")
+  const data = await res.json()
+
+  const grouped = {}
+  data.forEach(s => {
+    const date = s.date
+    if (!grouped[date]) {
+      grouped[date] = 0
+    }
+    grouped[date]++
+  })
+ stats.value = Object.keys(grouped).map(date => ({
+    date,
+    count: grouped[date]
+  }))
+}
+onMounted(loadData)
+onActivated(loadData)
+</script>
+
 <template>
   <div>
     <h2>Statistics</h2>
-    <div v-for="n in 7" :key="n">
-      Date: ---- | Count: --
+
+    <div v-if="stats.length === 0">
+      No data available
+     </div>
+
+
+    <div v-for="day in stats" :key="day.date">
+    {{ day.date }} : {{ day.count }} sessions
     </div>
-  </div>
+    </div>
 </template>
-<script setup>
-</script>
